691adc0be995b73691222ca49a35ddea59d0d1b2
Satoshi <abgi@live.fr>
2018-12-28 14:45:12 +0100

    Mise jour de la page mot de passe oublié

--- mdp_oublie.php
+++ mdp_oublie.php
@@ -6,10 +6,16 @@
   <h1>Le site est en construction</h1>
 </header>
 
-<section class="mdp_oublie">
+<section>
 
-  <p>Si vous avez oublié votre mot de passe, le seul moyen que vous avez pour acceder au contenue du site est de vous créer un nouveau compte.
-  Merci de votre compréhension.</p>
+  <div class="mdp_oublie">
+    <p>
+      Si vous avez oublié votre mot de passe, le seul moyen que vous avez pour acceder au contenue du site est de vous créer un nouveau compte.
+      <div>
+      Merci de votre compréhension.
+    </p>
+  </div>
+
 
 </section>
 
