a0eca7d51bbbced1ed6c3c9be002208d0fb7620f
Satoshi <abgi@live.fr>
2018-12-29 21:23:48 +0100

    Création de la page profil.php

---
+++ profil.php
@@ -0,0 +1,26 @@
+<?php
+  include 'header.php';
+?>
+
+<header class="header-creation-quiz">
+  <h1>Pseudo</h1>
+</header>
+
+<section class="section-profil">
+
+  <p>Nombre de points : 300</p>
+
+  <p>Nombre de quiz créés : 20</p>
+
+  <p>Liste des quiz créés</p>
+
+  <p>Pourcentage de bonnes réponses global</p>
+
+  <p>Pourcentage de bonnes réponses détaillé par catégories</p>
+
+</section>
+
+
+<?php
+  include 'footer.php';
+?>
