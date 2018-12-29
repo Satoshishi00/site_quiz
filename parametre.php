7f7d9103fa16c95a586e6e3be928fe7182190636
Satoshi <abgi@live.fr>
2018-12-29 21:38:31 +0100

    Création fichier parametre.php

---
+++ parametre.php
@@ -0,0 +1,36 @@
+<?php
+  include 'header.php';
+?>
+
+<header class="header-creation-quiz">
+  <h1>Paramètres</h1>
+</header>
+
+<section class="section-parametres">
+
+  <h2>Pseudo</h2>
+
+  <div class="bloc">
+    <a href="#">Changer adresse mail</a>
+  </div>
+
+
+  <div class="bloc">
+    <a href="#">Changer mot de passe</a>
+  </div>
+
+  <div class="bloc">
+    <p>
+      <a href="#">Réinitialiser le compte</a>
+      <br>
+      Cette opération réinitialise les points à 0 ainsi que toutes les statistiques
+    </p>
+  </div>
+
+
+</section>
+
+
+<?php
+  include 'footer_connexion.php';
+?>
