b7c1d230c01cb66714e280ed2619c3189a79ceb8
Satoshi <abgi@live.fr>
2018-12-30 04:46:38 +0100

    Transfert du traitement du formulaire d'inscription dans le fichier traitement-inscription.php et création du fichier deconnexion.php

---
+++ back/traitement-inscription.php
@@ -0,0 +1,93 @@
+<?php
+
+//connexion à la bdd
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=site_quiz;charset=utf8', 'root', '');
+}
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}
+
+//initialisation des erreurs
+$error_pseudo='';
+$error_mail='';
+$error_mdp='';
+
+//initialisation des variables
+$pseudo = '';
+$mail1 = '';
+$mail2 = '';
+$mdp1 = '';
+$mdp2 = '';
+
+if(!empty($_POST)){
+  $pseudo = $_POST["pseudo"];
+  $mail1 = $_POST['email1'];
+  $mail2 = $_POST['email2'];
+  $mdp1 = $_POST['mdp1'];
+  $mdp2 = $_POST['mdp2'];
+
+  //test si pseudo existe dans la table
+  $req = $bdd->prepare("SELECT pseudo FROM utilisateurs WHERE pseudo = :pseudo");
+  $req->execute(array('pseudo' => $pseudo));
+  $donnees = $req->fetch();
+  if(!$donnees){ //si le nb de fois qu'apparait $pseudo dans la table est différent de 0
+    if(strlen($pseudo)>=3){
+      //test si email existe dans la table
+      $req = $bdd->prepare("SELECT mail FROM utilisateurs WHERE mail = :mail");
+      $req->execute(array('mail' => $mail1));
+      $donnees = $req->fetch();
+      if(!$donnees){ //si le nb de fois qu'apparait $mail1 dans la table est différent de 0
+        if($mail1 === $mail2){
+          if(strlen($mdp1)>=6){
+            if($mdp1 === $mdp2){
+              $pass_hache = password_hash($mdp1, PASSWORD_DEFAULT);
+              $req = $bdd->prepare('INSERT INTO utilisateurs(pseudo, mail, mdp, date_inscription)
+                                    VALUES(:pseudo, :mail, :mdp, NOW())');
+              $req->execute(array(
+                                    'pseudo' => $pseudo,
+                                    'mail' => $mail1,
+                                    'mdp' => $pass_hache
+                                    ));
+              //début de la session
+              session_start();
+
+              $_SESSION['pseudo']       = $_POST["pseudo"];
+              //récupération de ip de l'utilisateur en session
+              $_SESSION['user_ip']      = $_SERVER['REMOTE_ADDR'];
+
+              header('Location: ../index.php');
+              exit();
+
+            }
+            else{
+              $error_mdp = "Les mots de passes saisis sont différents";
+            }
+          }
+          else{
+            $error_mdp = "Votre mot de passe doit faire au moins 6 caractères";
+          }
+        }
+        else{
+          $error_mail = "Les emails de passes sont différentes";
+        }
+      }
+      else{
+        $error_mail = "Cet email existe déjà";
+      }
+    }
+    else{
+      $error_pseudo = "Votre pseudo doit être compris entre 3 et 20 caractères";
+    }
+  }
+  else{
+    $error_pseudo = "Votre pseudo existe déjà";
+  }
+}
+else{
+  $error = "Veuillez remplir tous les champs";
+}
+
+?>
--- inscription.php
+++ inscription.php
@@ -2,91 +2,13 @@
   include 'html/header_connexion.html';
 ?>
 
-<?php
-try
-{
-	$bdd = new PDO('mysql:host=localhost;dbname=site_quiz;charset=utf8', 'root', '');
-}
-catch (Exception $e)
-{
-        die('Erreur : ' . $e->getMessage());
-}
 
+<?php
+
+//initialisation des erreurs
 $error_pseudo='';
 $error_mail='';
 $error_mdp='';
-
-$pseudo = '';
-$mail1 = '';
-$mail2 = '';
-$mdp1 = '';
-$mdp2 = '';
-
-if(!empty($_POST)){
-  $pseudo = $_POST["pseudo"];
-  $mail1 = $_POST['email1'];
-  $mail2 = $_POST['email2'];
-  $mdp1 = $_POST['mdp1'];
-  $mdp2 = $_POST['mdp2'];
-
-  $req = $bdd->prepare("SELECT pseudo FROM utilisateurs WHERE pseudo = :pseudo");
-  $req->execute(array('pseudo' => $pseudo));
-  $donnees = $req->fetch();
-  if(!$donnees){ //si le nb de fois qu'apparait $pseudo dans la table est différent de 0
-    if(strlen($pseudo)>=3){
-      $req = $bdd->prepare("SELECT mail FROM utilisateurs WHERE mail = :mail");
-      $req->execute(array('mail' => $mail1));
-      $donnees = $req->fetch();
-      if(!$donnees){ //si le nb de fois qu'apparait $mail1 dans la table est différent de 0
-        if($mail1 === $mail2){
-          if(strlen($mdp1)>=6){
-            if($mdp1 === $mdp2){
-              $pass_hache = password_hash($mdp1, PASSWORD_DEFAULT);
-              $req = $bdd->prepare('INSERT INTO utilisateurs(pseudo, mail, mdp, date_inscription)
-                                    VALUES(:pseudo, :mail, :mdp, NOW())');
-              $req->execute(array(
-                                    'pseudo' => $pseudo,
-                                    'mail' => $mail1,
-                                    'mdp' => $pass_hache
-                                    ));
-              session_start();
-
-              $_SESSION['pseudo']       = $_POST["pseudo"];
-
-              $_SESSION['user_ip']      = $_SERVER['REMOTE_ADDR'];
-
-            }
-            else{
-              $error_mdp = "Les mots de passes saisis sont différents";
-            }
-          }
-          else{
-            $error_mdp = "Votre mot de passe doit faire au moins 6 caractères";
-          }
-        }
-        else{
-          $error_mail = "Les emails de passes sont différentes";
-        }
-      }
-      else{
-        $error_mail = "Cet email existe déjà";
-      }
-    }
-    else{
-      $error_pseudo = "Votre pseudo doit être compris entre 3 et 20 caractères";
-    }
-  }
-  else{
-    $error_pseudo = "Votre pseudo existe déjà";
-  }
-}
-else{
-  $error = "Veuillez remplir tous les champs";
-}
-
-
-
-// echo 'Vous êtes inscrit ! ';
 
 ?>
 
@@ -96,7 +18,7 @@ else{
 
 <section class="section-inscription">
 
-  <form class="form-inscription" method="post">
+  <form class="form-inscription" method="post" action="back/traitement-inscription.php">
 
     <div class="form-group">
       <label for="exampleInputEmail1">Pseudo</label>
