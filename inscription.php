9ae04d44b2b6c7f906eea46d2ade9faf50dd09cc
Satoshi <abgi@live.fr>
2018-12-30 04:32:28 +0100

    maj requete du form d'inscription

--- inscription.php
+++ inscription.php
@@ -14,42 +14,70 @@ catch (Exception $e)
 
 $error_pseudo='';
 $error_mail='';
-$pass_hache = password_hash($_POST['mdp1'], PASSWORD_DEFAULT);
+$error_mdp='';
+
+$pseudo = '';
+$mail1 = '';
+$mail2 = '';
+$mdp1 = '';
+$mdp2 = '';
 
 if(!empty($_POST)){
-  if(strlen($_POST['pseudo'])>=3){
-    $req = $bdd->prepare("SELECT mail FROM utilisateurs WHERE mail=mail");
-    $req->execute(array($_POST['mail1']));
-    $donnees = $req->fetch();
-    if(!$donnees){ //si le nombre de mail dans la table est différent de 0
-      if($_POST['email1'] === $_POST['email2']){
-        if(strlen($_POST['mdp1'])>=6){
-          if($_POST['mdp1'] === $_POST['mdp2']){
-            $req = $bdd->prepare('INSERT INTO utilisateurs(pseudo, mail, mdp, date_inscription)
-                                  VALUES(:pseudo, :mail, :mdp, NOW())');
-            $req->execute(array(
-                                  'pseudo' => $_POST["pseudo"],
-                                  'mail' => $_POST['email1'],
-                                  'mdp' => $pass_hache
-                                  ));
+  $pseudo = $_POST["pseudo"];
+  $mail1 = $_POST['email1'];
+  $mail2 = $_POST['email2'];
+  $mdp1 = $_POST['mdp1'];
+  $mdp2 = $_POST['mdp2'];
+
+  $req = $bdd->prepare("SELECT pseudo FROM utilisateurs WHERE pseudo = :pseudo");
+  $req->execute(array('pseudo' => $pseudo));
+  $donnees = $req->fetch();
+  if(!$donnees){ //si le nb de fois qu'apparait $pseudo dans la table est différent de 0
+    if(strlen($pseudo)>=3){
+      $req = $bdd->prepare("SELECT mail FROM utilisateurs WHERE mail = :mail");
+      $req->execute(array('mail' => $mail1));
+      $donnees = $req->fetch();
+      if(!$donnees){ //si le nb de fois qu'apparait $mail1 dans la table est différent de 0
+        if($mail1 === $mail2){
+          if(strlen($mdp1)>=6){
+            if($mdp1 === $mdp2){
+              $pass_hache = password_hash($mdp1, PASSWORD_DEFAULT);
+              $req = $bdd->prepare('INSERT INTO utilisateurs(pseudo, mail, mdp, date_inscription)
+                                    VALUES(:pseudo, :mail, :mdp, NOW())');
+              $req->execute(array(
+                                    'pseudo' => $pseudo,
+                                    'mail' => $mail1,
+                                    'mdp' => $pass_hache
+                                    ));
+              session_start();
+
+              $_SESSION['pseudo']       = $_POST["pseudo"];
+
+              $_SESSION['user_ip']      = $_SERVER['REMOTE_ADDR'];
+
+            }
+            else{
+              $error_mdp = "Les mots de passes saisis sont différents";
+            }
           }
           else{
-            $error_mdp = "Les mots de passes saisis sont différents";
+            $error_mdp = "Votre mot de passe doit faire au moins 6 caractères";
           }
         }
         else{
-          $error_mdp = "Votre mot de passe doit faire au moins 6 caractères";
+          $error_mail = "Les emails de passes sont différentes";
         }
       }
       else{
-        $error_mail = "Les emails de passes sont différentes";
+        $error_mail = "Cet email existe déjà";
       }
+    }
     else{
-      $error_mail = "Cet email existe déjà";
+      $error_pseudo = "Votre pseudo doit être compris entre 3 et 20 caractères";
     }
   }
   else{
-    $error_pseudo = "Votre pseudo doit être compris entre 3 et 20 caractères";
+    $error_pseudo = "Votre pseudo existe déjà";
   }
 }
 else{
@@ -88,8 +116,8 @@ else{
     <div class="form-group">
       <label for="exampleInputPassword1">Mot de passe</label>
       <input type="password" class="form-control" name="mdp1" placeholder="Entrez votre mot de passe" maxlength="23" required>
-      <small id="passwordlHelp" class="form-text text-muted">Votre mot de passe doit faire entre 6 et 23 caractères et doit contenir au moins une lettre et un chiffre</small>
-    </div>
+      <small class="small-error"><?= $error_mdp ?></small>
+      </div>
     <div class="form-group">
       <label for="exampleInputPassword1">Confirmation mot de passe</label>
       <input type="password" class="form-control" name="mdp2" placeholder="Confirmez votre mot de passe" maxlength="23" required>
