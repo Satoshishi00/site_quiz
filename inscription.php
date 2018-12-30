44a866b8c5d4147cab66fb76792200b3731126ee
Satoshi <abgi@live.fr>
2018-12-30 04:03:34 +0100

    Contrôle et ajout à la bdd du formulaire d'inscription du fichier inscription.php dans le fichier inscription.php

--- inscription.php
+++ inscription.php
@@ -12,37 +12,51 @@ catch (Exception $e)
         die('Erreur : ' . $e->getMessage());
 }
 
+$error_pseudo='';
+$error_mail='';
 $pass_hache = password_hash($_POST['mdp1'], PASSWORD_DEFAULT);
 
 if(!empty($_POST)){
-  if(!empty($_POST['pseudo'])){
-    if(strlen($_POST['pseudo'])>=3 AND strlen($_POST['pseudo'])<=20){
-      if(!empty($_POST['mail1'])){
-
+  if(strlen($_POST['pseudo'])>=3){
+    $req = $bdd->prepare("SELECT mail FROM utilisateurs WHERE mail=mail");
+    $req->execute(array($_POST['mail1']));
+    $donnees = $req->fetch();
+    if(!$donnees){ //si le nombre de mail dans la table est différent de 0
+      if($_POST['email1'] === $_POST['email2']){
+        if(strlen($_POST['mdp1'])>=6){
+          if($_POST['mdp1'] === $_POST['mdp2']){
+            $req = $bdd->prepare('INSERT INTO utilisateurs(pseudo, mail, mdp, date_inscription)
+                                  VALUES(:pseudo, :mail, :mdp, NOW())');
+            $req->execute(array(
+                                  'pseudo' => $_POST["pseudo"],
+                                  'mail' => $_POST['email1'],
+                                  'mdp' => $pass_hache
+                                  ));
+          }
+          else{
+            $error_mdp = "Les mots de passes saisis sont différents";
+          }
+        }
+        else{
+          $error_mdp = "Votre mot de passe doit faire au moins 6 caractères";
+        }
       }
       else{
-        $error_mail1 = "Rentrez une adresse mail"
+        $error_mail = "Les emails de passes sont différentes";
       }
-    }
     else{
-      $error = "Votre pseudo doit être compris entre 3 et 20 caractères";
+      $error_mail = "Cet email existe déjà";
     }
   }
   else{
-    $error = "Veuillez Saisir un pseudo";
+    $error_pseudo = "Votre pseudo doit être compris entre 3 et 20 caractères";
   }
 }
 else{
   $error = "Veuillez remplir tous les champs";
 }
 
-$req = $bdd->prepare('INSERT INTO utilisateurs(pseudo, mail, mdp, date_inscription)
-                      VALUES(:pseudo, :mail, :mdp, NOW())');
-$req->execute(array(
-                      'pseudo' => $_POST["pseudo"],
-                      'mail' => $_POST['email1'],
-                      'mdp' => $pass_hache
-                      ));
+
 
 // echo 'Vous êtes inscrit ! ';
 
@@ -58,25 +72,27 @@ $req->execute(array(
 
     <div class="form-group">
       <label for="exampleInputEmail1">Pseudo</label>
-      <input type="text" class="form-control" name="pseudo" placeholder="Entrez un pseudo">
-      <small class="small-error"><?= $error ?></small>
+      <input type="text" class="form-control" name="pseudo" placeholder="Entrez un pseudo" min="3" maxlength="20" required>
+      <small class="small-error"><?= $error_pseudo ?></small>
     </div>
     <div class="form-group">
       <label for="exampleInputEmail1">Email</label>
-      <input type="email" class="form-control" name="email1" aria-describedby="emailHelp" placeholder="Entrez votre email">
+      <input type="email" class="form-control" name="email1" aria-describedby="emailHelp" placeholder="Entrez votre email" required>
+      <small class="small-error"><?= $error_mail ?></small>
     </div>
     <div class="form-group">
       <label for="exampleInputEmail1">Corfirmation Email</label>
-      <input type="email" class="form-control" name="email2" aria-describedby="emailHelp" placeholder="Confirmez votre email">
+      <input type="email" class="form-control" name="email2" aria-describedby="emailHelp" placeholder="Confirmez votre email" required>
+      <small class="small-error"><?= $error_mail ?></small>
     </div>
     <div class="form-group">
       <label for="exampleInputPassword1">Mot de passe</label>
-      <input type="password" class="form-control" name="mdp1" placeholder="Entrez votre mot de passe">
+      <input type="password" class="form-control" name="mdp1" placeholder="Entrez votre mot de passe" maxlength="23" required>
       <small id="passwordlHelp" class="form-text text-muted">Votre mot de passe doit faire entre 6 et 23 caractères et doit contenir au moins une lettre et un chiffre</small>
     </div>
     <div class="form-group">
       <label for="exampleInputPassword1">Confirmation mot de passe</label>
-      <input type="password" class="form-control" name="mdp2" placeholder="Confirmez votre mot de passe">
+      <input type="password" class="form-control" name="mdp2" placeholder="Confirmez votre mot de passe" maxlength="23" required>
     </div>
 
 
